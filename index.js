a8ee8fb98bf65db9d420a9edc4293cd42e96f6f3
michaelt0802 <michaelt0802@gmail.com>
2023-06-08 14:52:27 -0700

    implemented error handling. rewriting an entry using a name already in DB now doesn't crash site

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
 morgan.token('body', (req, res) => {
   if(req.method === 'POST') {
     return JSON.stringify(req.body)
@@ -59,15 +73,32 @@ app.post('/api/persons/', async (req, res) => {
   res.json(await person.save())
 })
 
-app.delete('/api/persons/:id', async (req, res) => {
+app.put('/api/persons/:id', async (req, res, next) => {
+  try {
+    const body = req.body
+    console.log('body', body)
+    const person = {
+      name: body.name,
+      number: body.number,
+      _id: body._id,
+    }
+    res.json(await Person.findByIdAndUpdate(req.params.id, person, { new: true }))
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.delete('/api/persons/:id', async (req, res, next) => {
   try {
     await Person.findByIdAndRemove(req.params.id)
     res.status(204).end()
   } catch (error) {
-    console.log('error', error)
-    throw(error)
+    next(error)
   }
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
